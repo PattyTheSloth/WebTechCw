e9d0c54a75c060bd05e703a4475c8b104db62cff
WackaC00 <40690922@live.napier.ac.uk>
2025-04-24 15:58:01 +0100

    html/js changes

--- quizzes/Wscript.js
+++ quizzes/Wscript.js
@@ -1,7 +1,7 @@
 // Quiz questions
 const questions = [
   {
-    image: "Images/Obama.jpg",
+    image: "images/Obama.jpg",
     question: "Can you guess who this really is?",
     hint: "This person was the 44th President of the USA",
     answers: [
@@ -12,7 +12,7 @@ const questions = [
     ]
   },
   {
-    image: "Images/LeoDiCap.jpg",
+    image: "images/LeoDiCap.jpg",
     question: "Who is this famous actor?",
     hint: "He won an Oscar for The Revenant",
     answers: [
@@ -23,7 +23,7 @@ const questions = [
     ]
   },
   {
-    image: "Images/MarkZuk.jfif",
+    image: "images/MarkZuk.jfif",
     question: "Identify this tech entrepreneur",
     hint: "He founded Facebook",
     answers: [
@@ -34,8 +34,8 @@ const questions = [
     ]
   },
   {
-    image: "Images/ForrestWhittaker.jpg",
-    question: "Who could this be",
+    image: "images/ForrestWhittaker.jpg",
+    question: "Who could this be?",
     hint: "He starred in star wars",
     answers: [
       {text: "Kenn Whittaker", correct: false},
@@ -45,8 +45,8 @@ const questions = [
     ]
   },
   {
-    image: "Images/WillSmith.jpg",
-    question: "Who could this be",
+    image: "images/WillSmith.jpg",
+    question: "Can you guess who this is?",
     hint: "His son played the karate kid",
     answers: [
       {text: "Alfonso Riberio", correct: false},
